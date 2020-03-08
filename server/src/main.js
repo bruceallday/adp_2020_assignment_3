19379a4f5ec8f268530f0006b4b7fd196986a2de
Bruce Pouncey <bruce.pouncey@yahoo.com>
2020-03-07 19:30:35 -0500

    feat: Added initial createNewUser function in main.js in server

--- server/src/main.js
+++ server/src/main.js
@@ -1,4 +1,17 @@
-const { ApolloServer, gql } = require('apollo-server');
+import { ApolloServer, gql } from 'apollo-server'
+import { Pool } from 'pg' 
+import bcrypt from 'bcrypt';
+import csurf from 'csurf';
+import cors from 'cors';
+
+const pool = new Pool()
+
+const registerNewUser = (password) => {
+  const saltRounds = 10
+  const hash = bcrypt.hash(password, saltRounds)
+  return hash
+
+}
 
 // The GraphQL schema
 const typeDefs = gql`
