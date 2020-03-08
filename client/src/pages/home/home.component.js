97fd5a804bf7cb5f655afc15939a60ad73b648ca
Bruce Pouncey <bruce.pouncey@yahoo.com>
2020-03-08 15:38:11 -0400

    style: Added styling to Homepage Component

--- client/src/pages/home/home.component.js
+++ client/src/pages/home/home.component.js
@@ -1,12 +1,24 @@
 import React from 'react'
 import Header from '../../components/header/header.component'
 
+import Paper from '@material-ui/core/Paper'
+import Typography from '@material-ui/core/Typography'
+
+import useStyles from './home.styles'
+
 const Home = () => {
+
+    const classes = useStyles()
 
     return(
         <div>
             <Header />
-                HOME PAGE           
+            <Paper className={classes.root} >
+                <Typography variant="h1" gutterBottom >
+                    Welcome!
+                </Typography>
+            </Paper>
+                           
         </div>
     )
 }
---
+++ client/src/pages/home/home.styles.js
@@ -0,0 +1,10 @@
+import { makeStyles } from '@material-ui/styles/'
+
+const useStyles = makeStyles({
+  root: {
+    height: '90vh',
+    padding: 30,
+  },
+});
+
+export default useStyles;
